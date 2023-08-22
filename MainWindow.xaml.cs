b823f6b660cb1d910abfee0e793a13ee6c326a80
Ianuarius <juha.keranen85@gmail.com>
2023-08-22 22:03:49 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace WinUIBasics
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            double conversionRate = 1.12;
+            double.TryParse(amountTextBox.Text, out double amount);
+            double convertedAmount = amount * conversionRate;
+            outputTextBlock.Text = "Converted Amount: " + convertedAmount.ToString("F2") + " EUR";
+
+        }
+    }
+}
